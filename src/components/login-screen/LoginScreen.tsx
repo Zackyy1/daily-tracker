edbb9c6499f86ba1c1b3b440a87fc46ea6ae9f6e
Martin <qwertyuk01@gmail.com>
2024-06-23 17:39:49 +0300

    wip authentication

--- src/components/login-screen/LoginScreen.tsx
+++ src/components/login-screen/LoginScreen.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { auth } from '@/config/firebase';
+import { Button } from '../buttons/Button';
+import {
+	LoginWithGoogle,
+	getCurrentUser,
+	logout,
+} from '@/utils/authentication/AuthenticationUtils';
 
 export const LoginScreen = () => {
-  return (
-	<div>LoginScreen</div>
-  )
-}
+
+  console.log(auth)
+	return (
+		<div>
+			{!!getCurrentUser() ? (
+				<Button onClick={() => logout(auth)} text='Logout' />
+			) : (
+				<Button onClick={LoginWithGoogle} text='Login' />
+			)}
+		</div>
+	);
+};
--- src/utils/authentication/AuthenticationUtils.ts
+++ src/utils/authentication/AuthenticationUtils.ts
@@ -1,26 +1,53 @@
-import { FB_GoogleProvider, auth } from "@/config/firebase";
+import { FB_GoogleProvider, auth } from '@/config/firebase';
 import {
-  getAuth,
-  GoogleAuthProvider,
-  type User,
-  onAuthStateChanged,
-  signInWithPopup,
-} from "firebase/auth";
+	type User,
+	onAuthStateChanged,
+	signInWithPopup,
+	signOut,
+	type Auth,
+} from 'firebase/auth';
+
+export type TAuthState = {
+	uid: string;
+	name: string;
+};
 
 export const LoginWithGoogle = () => {
-  signInWithPopup(auth, FB_GoogleProvider).then((result) => {
-    console.log("Yeet");
-  });
+	signInWithPopup(auth, FB_GoogleProvider).then((result) => {});
 };
-export const onAuthChanged = (user: User) =>
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/auth.user
-      const uid = user.uid;
-      // ...
-    } else {
-      // User is signed out
-      // ...
-    }
-  });
+
+const extractAuthData = (authUser: User) => {
+	return {
+		name: authUser.displayName ?? 'friend',
+		uid: authUser.uid,
+	};
+};
+
+export const logout = (auth: Auth) => {
+	signOut(auth)
+		.then((result) => {
+			console.log('Logged out', result);
+		})
+		.catch((err) => console.error(err));
+};
+
+export const getCurrentUser = () => {
+	if (!auth.currentUser?.uid) return false;
+	return extractAuthData(auth.currentUser);
+};
+
+export const onAuthChanged = () =>
+	onAuthStateChanged(auth, (user): TAuthState | undefined => {
+		if (user) {
+			// User is signed in, see docs for a list of available properties
+			// https://firebase.google.com/docs/reference/js/auth.user
+			const uid = user.uid;
+			// ...
+			return extractAuthData(user);
+		} else {
+			// User is signed out
+			// ...
+			console.log('Auth changed: logged out', user);
+			return;
+		}
+	});
